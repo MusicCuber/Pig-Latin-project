ea50fdf3e4786a3ea95607fe132beb21b49d9795
Elijah Guan <71979603+MusicCuber@users.noreply.github.com>
2024-11-20 01:35:35 +0000

    Initial commit

--- src/App.java
+++ src/App.java
@@ -1,7 +1,11 @@
 class App {
   public static void main(String[] args) {
-    Book aBook = new Book();
-    System.out.println(aBook.pigLatin("hello"));
-    System.out.println(aBook.translateWord(""));
+    Book aBook = new Book("https://www.gutenberg.org/cache/epub/74696/pg74696.txt");
+    Book sosBook = new Book(" https://www.gutenberg.org/cache/epub/64317/pg64317.txt");
+    
+  //  aBook.writeToFile(aBook.pigLatin(aBook.returnBook()));
+  //  System.out.println("Word count for myBook: " + aBook.wordCount());
+   sosBook.writeToFile(sosBook.pigLatin(sosBook.returnBook()));
+   System.out.println("Word count for sosBook: " + sosBook.wordCount());
   }
 }
--- src/Book.java
+++ src/Book.java
@@ -1,34 +1,162 @@
-//A few assumptions.......
+import java.io.IOException;
+import java.io.InvalidObjectException;
+import java.util.Scanner;
+import java.net.URL;
+import java.io.FileWriter;   // Import the FileWriter class
+import java.io.IOException;  // Import the IOException class to handle errors
 
-//Words will be separated by spaces. 
-//There can be punctuation in a word, we will only add/keep punctuation at the end of a string if it is at the end of a string.
-//    for examples: Hello.==> Ellohay.    Good-bye! ==> Ood-byegay!    so... ==> osay...
+public class Book {
+    private int totalWords;
+    private String bookText = "";
+    public Book(String link) {
+        totalWords = 0;
+        
+        readBook(link);
+    }
+    
 
-public class Book
-{
-  public String pigLatin(String word)
-  {
+  public void writeToFile(String text) {
+    try {
+      FileWriter myWriter = new FileWriter("filename.txt");
+      myWriter.write(text);
+      myWriter.close();
+      System.out.println("Successfully wrote to the file.");
+    } catch (IOException e) {
+      System.out.println("An error occurred.");
+      e.printStackTrace();
+    }
+  }
+
+    private void readBook(String link) {
+        try {
+            URL url = new URL(link);
+            Scanner s = new Scanner(url.openStream());
+            boolean toTranslate = false;
+            while (s.hasNext()) {
+                
+                String text = s.nextLine();
+                if (text.contains("START OF THE PROJECT GUTENBERG EBOOK")) {
+                    toTranslate = true;
+                    continue;
+                }
+                else if (text.contains("END OF THE PROJECT GUTENBERG EBOOK")) {
+                    break;
+                }
+                if (toTranslate) {
+                    bookText += text;
+
+                    String[] words = text.split(" ");
+                    totalWords += words.length;
+                }
+                
+            }
+        }
+        catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+  private static final String vowels = "aeiou";
+  private static final String capitals = "QWERTYUIOPASDFGHJKLZXCVBNM";
+  private static final String PUNCTUATION = "!@#$%^&*()-<>?:,./[]`~";
+  public String returnBook() {
+    return bookText;
+  }
+  public String pigLatin(String sentence) {
+      return translateSentence(bookText);
+  }
+
+  public String translateSentence(String sentence) {
+      String[] wordList = sentence.split(" "); // Split the sentence into words
+      String ret = "";
+      
+      for (String word : wordList) {
+        if (word.length() == 0) {
+            continue;
+        }
+        else {
+            ret += (translateWord(word)) + " ";
+        
+          
+            }
+        }
+      return ret.trim(); // Remove trailing space
 
   }
+  public String translateWord(String word) {
+    String cleanWord = word;
+    String endPunctuation = extractEndPunctuation(cleanWord);
+    
+    // Check if cleanWord is empty before accessing its first character
+    boolean isCapital = cleanWord.length() > 0 && Character.isUpperCase(cleanWord.charAt(0));
+
+    String pigLatinWord = convertToPigLatin(cleanWord);
+
+    if (isCapital) {
+        pigLatinWord = capitalizeFirstLetter(pigLatinWord);
+    }
+
+    return pigLatinWord + endPunctuation;
+}
+
+  private String extractEndPunctuation(String word) {
+      String punctuation = "";
+      while (word.length() > 0 && PUNCTUATION.indexOf(word.charAt(word.length() - 1)) >= 0) {
+        punctuation += word.charAt(word.length() - 1);
+        word = word.substring(0, word.length() - 1);
+    }
+      return punctuation;
+  }
+
+  private String convertToPigLatin(String word) {
+    if (word.length() == 0) {
+        return word; // Handle empty string
+    }
+    
+    // Remove trailing punctuation
+    while (word.length() > 0 && PUNCTUATION.indexOf(word.charAt(word.length() - 1)) >= 0) {
+        word = word.substring(0, word.length() - 1);
+    }
+
+    if (word.length() == 0) {
+        return ""; // Return empty if only punctuation was present
+    }
+
+    String firstLetter = word.substring(0, 1);
+    String lowerFirstLetter = firstLetter.toLowerCase();
+    String pigLatin;
+
+    if (vowels.indexOf(lowerFirstLetter) >= 0) {
+        pigLatin = word + "yay";
+    } else if (word.length() > 1 && vowels.indexOf(word.charAt(1)) >= 0) {
+        pigLatin = word.substring(1) + lowerFirstLetter + "ay";
+    } else if (word.length() > 2 && vowels.indexOf(word.charAt(2)) >= 0) {
+        pigLatin = word.substring(2) + lowerFirstLetter + word.charAt(1) + "ay";
+    } else {
+        pigLatin = word + "ay"; // Handle cases where the word is too short
+    }
+
+    return pigLatin;
+}
   
-  public int endPunctuation(String word)  //return the index of where the punctuation is at the end of a String. If it is all punctuation return 0, if there is no punctuation return -1
-  {
 
-    return -1;
+  
+  private String capitalizeFirstLetter(String word) {
+      return word.substring(0, 1).toUpperCase() + word.substring(1);
   }
 
-  public String translateWord(String word)    //to share with class
-  {
-    String convertedWord = "";
-
-    return convertedWord;
+  public int endPunctuation(String word) {
+      int index = -1;
+      for (int i = word.length() - 1; i >= 0; i--) {
+          if (PUNCTUATION.indexOf(word.charAt(i)) < 0) {
+              // Found a non-punctuation character
+              index = i + 1; // Return the index of the first non-punctuation character
+              break;
+          }
+      }
+      return -1; // If all punctuation, return 0; if none, return -1
   }
-
-  public String translateSentence(String sentence)
-  {
-    String retSentence = "";
-
-
-    return retSentence;
+  public int wordCount() {
+    return totalWords;
   }
-}  
+}
+
